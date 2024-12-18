df539de688e2b0ec420c01ee1008040b8031d910
Fghori <fghori02@gmail.com>
2024-12-18 23:10:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 
 # Load the dataset
 
